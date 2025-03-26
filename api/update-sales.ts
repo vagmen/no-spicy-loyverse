ed38b3508098a745ec5121fded86a745dffe4e95
Vagiz Akhmetov <vagmen27@gmail.com>
2025-03-26 17:06:10 +0300

    Обновлено описание проекта в package.json, удалены настройки cron из vercel.json, и улучшена обработка ошибок в API для обновления данных, включая проверку на авторизацию и возврат деталей ошибки.

--- api/update-sales.ts
+++ api/update-sales.ts
@@ -2,17 +2,23 @@ import { main } from "../src";
 
 export default async function handler(req: any, res: any) {
   try {
-    // Проверяем, что запрос пришел от Vercel Cron
-    const isVercelCron = req.headers["x-vercel-cron"] === "1";
+    // Проверяем, что запрос пришел от cron-job.org или локально
+    const userAgent = req.headers["user-agent"] || "";
+    const isCronJob =
+      userAgent.includes("Cron-Job.org") ||
+      req.headers["x-vercel-cron"] === "1";
 
-    if (!isVercelCron) {
+    if (!isCronJob && process.env.NODE_ENV === "production") {
       return res.status(401).json({ error: "Unauthorized" });
     }
 
     await main();
-    res.status(200).json({ message: "Данные успешно обновлены" });
-  } catch (error) {
+    return res.status(200).json({ message: "Данные успешно обновлены" });
+  } catch (error: any) {
     console.error("Ошибка при обновлении данных:", error);
-    res.status(500).json({ error: "Ошибка при обновлении данных" });
+    return res.status(500).json({
+      error: "Ошибка при обновлении данных",
+      details: error.message,
+    });
   }
 }
